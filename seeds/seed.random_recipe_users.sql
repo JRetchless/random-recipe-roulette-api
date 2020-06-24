81a7096531103ae6ba7790b244395587d3aba18f
Jonathan <jonretchless@gmail.com>
2020-06-24 16:41:54 -0700

    creating migrate/seeds files

---
+++ seeds/seed.random_recipe_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO groupcheck_users (firstname, lastname, email, p_word)
+VALUES
+    ('Sam', 'Gamgee', 'sam.gamgee@shire.com', 'password'),
+    ('Peregrin', 'Took', 'peregrin.took@shire.com', 'password'),
+    ('Saruman', 'The White', 'saruman.thewhite@isengard.com', 'password')
