e1094f9e52a39e8938e0c936231e5b4c7f7f199f
Jonathan <jonretchless@gmail.com>
2020-06-30 10:09:31 -0700

    pre merge

--- Script/script2.js
+++ Script/script2.js
@@ -15,17 +15,21 @@ function my_escape(sql){
 let keys = Object.keys(recipes)
 console.log(keys)
 for(let i =0; i<keys.length; i++){
-    // if(recipes[keys[i]].ingredients && recipes[keys[i]].ingredients['length']){
-    //     for(let j=0;j<recipes[keys[i]].ingredients['length'];j++){
-    //       recipes[keys[i]].ingredients[j].trim()
-    //     }
-    // }
-    console.log(`(,${my_escape(recipes[keys[i]].ingredients.join())}),`);
-//     sql+=`(${my_escape(recipes[keys[i]].name)},${my_escape(recipes[keys[i]].source)},${recipes[keys[i]].preptime},
-//     ${recipes[keys[i]].waittime}, ${recipes[keys[i]].cooktime},${recipes[keys[i]].servings},
-//     ${my_escape(recipes[keys[i]].comments)},${recipes[keys[i]].calories},${recipes[keys[i]].fat},
-//     ${recipes[keys[i]].satfat},${recipes[keys[i]].carbs},${recipes[keys[i]].fiber},${recipes[keys[i]].sugar},
-//     ${recipes[keys[i]].protein},${my_escape(recipes[keys[i]].instructions)},
-//     ${my_escape(recipes[keys[i]].ingredients.join())},${my_escape(recipes[keys[i]].tags.join('\r'))}),`
+    if(recipes[keys[i]].ingredients && recipes[keys[i]].ingredients['length']){
+        for(let j=0;j<recipes[keys[i]].ingredients['length'];j++){
+          recipes[keys[i]].ingredients[j].trim()
+          recipes[keys[i]].ingredients[j] = recipes[keys[i]].ingredients[j].replace("\r", "")
+        }
+    }
+      console.log(recipes[keys[i]].ingredients);
+      let joined = recipes[keys[i]].ingredients.join('--')
+      console.log('Result: '+joined);
+    // console.log(recipes[keys[i]].ingredients);
+    sql+=`(${my_escape(recipes[keys[i]].name)},${my_escape(recipes[keys[i]].source)},${recipes[keys[i]].preptime},
+    ${recipes[keys[i]].waittime}, ${recipes[keys[i]].cooktime},${recipes[keys[i]].servings},
+    ${my_escape(recipes[keys[i]].comments)},${recipes[keys[i]].calories},${recipes[keys[i]].fat},
+    ${recipes[keys[i]].satfat},${recipes[keys[i]].carbs},${recipes[keys[i]].fiber},${recipes[keys[i]].sugar},
+    ${recipes[keys[i]].protein},${my_escape(recipes[keys[i]].instructions)},
+    ${my_escape(joined)},${my_escape(recipes[keys[i]].tags.join('\r'))}),`
 }
 // fs.writeFileSync('seed.recipes.sql', sql, {encoding:"utf8"})
